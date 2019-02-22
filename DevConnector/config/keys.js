cdf6bb54e5619979b450fc24c302afd20db3e0cf
mylvcs <wmy0828@nuaa.edu.cn>
2019-02-22 00:30:16 -0800

    222

--- DevConnector/config/keys.js
+++ DevConnector/config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    mongoURI:
-    'mongodb+srv://admin:kengdiewang@cluster0-bbhnv.mongodb.net/test?retryWrites=true'
-};
+if (process.env.NODE_ENV ==='production'){
+    module.exports = require('./keys_prod');
+}else {
+    module.exports= require('./keys_dev');
+}
---
+++ DevConnector/config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:
+    'mongodb+srv://admin:kengdiewang@cluster0-bbhnv.mongodb.net/test?retryWrites=true',
+    secretOrKey : 'secret'
+};
